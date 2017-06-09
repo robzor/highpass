04a845aa801a2cacb14dce50594a62942652d116
Rob Hodges <robhodges@gmail.com>
2017-06-09 01:23:12 +0100

    Add in project images and very basic layout ideas

--- public/index.php
+++ public/index.php
@@ -22,93 +22,107 @@
 		</div>
 	</header>
 
-	<main class="site-body site-wrapper site-wrapper--body-text" role="main">
-		<article class="project project--media-right" itemscope itemtype="http://schema.org/SoftwareApplication">
-			<h2 class="project__title" itemprop="name">Société Générale Execution</h2>
-			<div class="project__body">
-				<figure class="project__media">
-					<img class="" itemprop="image" src="" alt="Société Générale Execution">
-					<figcaption class="project__media-description">Société Générale Execution example</figcaption>
-				</figure>
-				<div class="project__copy editorial">
+	<main class="site-body" role="main">
+		<article class="project project--media-left" itemscope itemtype="http://schema.org/WebSite">
+			<figure class="project__media">
+				<a href="https://democracyclub.org.uk/" target="_blank"><img class="project__media-img" itemprop="image" src="/assets/site_images/democracy-club.jpg" alt="Democracy Club"></a>
+				<figcaption class="project__media-description">Democracy Club</figcaption>
+			</figure>
+			<div class="project__copy">
+				<h2 class="project__title" itemprop="name">Democracy Club</h2>
+				<div class="editorial">
+					<p>I worked with <a href="https://democracyclub.org.uk/" target="_blank">Democracy Club</a>, creators of <a href="https://whocanivotefor.co.uk/" target="_blank">Who Can I Vote For?</a> and <a href="https://wheredoivote.co.uk/" target="_blank">Where Can I Vote?</a> to restructure the information architecture of their website.</p>
+					<p>Wireframes were created using <a href="https://www.figma.com/" target="_blank">Figma</a>.</p>
+				</div>
+				<a class="project__link" href="https://democracyclub.org.uk/" itemprop="url" target="_blank">Democracy Club</a>
+			</div>
+		</article>
+		<!-- <article class="project project--media-right" itemscope itemtype="http://schema.org/SoftwareApplication">
+			<figure class="project__media">
+				<img class="project__media-img" itemprop="image" src="/assets/site_images/" alt="Société Générale Execution">
+				<figcaption class="project__media-description">Société Générale Execution example</figcaption>
+			</figure>
+			<div class="project__copy">
+				<h2 class="project__title" itemprop="name">Société Générale Execution</h2>
+				<div class="editorial">
 					<p>In conjunction with <a href="http://www.artstudio.london/" target="_blank">Art Studio.</a>, I created a bespoke CSS/HTML <a href="http://getbem.com/" target="_blank">BEM</a> framework for their trading platform application suite.</p>
 					<p>The CSS was written in <a href="http://sass-lang.com/" target="_blank">SASS</a>, and along with the JavaScript, compiled using the <a href="http://nodejs.org/" target="_blank">node</a> module <a href="http://gulpjs.com/" target="_blank">gulp</a>.</p>
 					<p>I authored a complete documentation and implementation guide using <a href="http://www.mkdocs.org/">MkDocs</a>, hosted on their internal intranet.</p>
 				</div>
 			</div>
-		</article>
+		</article> -->
 		<article class="project project--media-left" itemscope itemtype="http://schema.org/WebSite">
-			<h2 class="project__title" itemprop="name">Frank Sinatra 100</h2>
-			<div class="project__body">
-				<figure class="project__media">
-					<a href="http://sinatra.com/100facts/" target="_blank"><img class="" itemprop="image" src="" alt="Frank Sinatra 100"></a>
-					<figcaption class="project__media-description">Frank Sinatra 100</figcaption>
-				</figure>
-				<div class="project__copy editorial">
+			<figure class="project__media">
+				<a href="http://sinatra.com/100facts/" target="_blank"><img class="project__media-img" itemprop="image" src="/assets/site_images/sinatra-100.jpg" alt="Frank Sinatra 100"></a>
+				<figcaption class="project__media-description">Frank Sinatra 100</figcaption>
+			</figure>
+			<div class="project__copy">
+				<h2 class="project__title" itemprop="name">Frank Sinatra 100</h2>
+				<div class="editorial">
 					<p>With design from <a href="http://logologo.co.uk/" target="_blank">LogoLogo</a>, I created an interactive experience to celebrate Frank Sinatra's 100th birthday for <a href="http://www.universalmusic.com/" target="_blank">Universal Music Group</a>.</p>
 					<p>I utilised the <a href="http://scrollmagic.io/" target="_blank">ScrollMagic</a> and <a href="https://greensock.com/" target="_blank">GreenSock</a> JavaScript animation libraries to create a visually interesting and engaging experience.</p>
-					<a class="project__link" href="http://sinatra.com/100facts/" itemprop="url" target="_blank">Frank Sinatra 100</a>
 				</div>
+				<a class="project__link" href="http://sinatra.com/100facts/" itemprop="url" target="_blank">Frank Sinatra 100</a>
 			</div>
 		</article>
 		<article class="project project--media-right" itemscope itemtype="http://schema.org/WebSite">
-			<h2 class="project__title" itemprop="name">National Housing Federation</h2>
-			<div class="project__body">
-				<figure class="project__media">
-					<a href="http://www.housing.org.uk/" target="_blank"><img class="" itemprop="image" src="" alt="National Housing Federation"></a>
-					<figcaption class="project__media-description">National Housing Federation</figcaption>
-				</figure>
-				<div class="project__copy editorial">
+			<figure class="project__media">
+				<a href="http://www.housing.org.uk/" target="_blank"><img class="project__media-img" itemprop="image" src="/assets/site_images/national-housing-federation.jpg" alt="National Housing Federation"></a>
+				<figcaption class="project__media-description">National Housing Federation</figcaption>
+			</figure>
+			<div class="project__copy">
+				<h2 class="project__title" itemprop="name">National Housing Federation</h2>
+				<div class="editorial">
 					<p>Working with <a href="http://electricputty.co.uk/" target="_blank">Electric Putty</a>, I led the development of the National Housing Federation's new website.</p>
 					<p>Built with <a href="http://expressionengine.com/" target="_blank">ExpressionEngine</a>, I developed the architecture for their wealth of content, including a members-only resource section.</p>
-					<a class="project__link" href="http://www.housing.org.uk/" itemprop="url" target="_blank">National Housing Federation</a>
 				</div>
+				<a class="project__link" href="http://www.housing.org.uk/" itemprop="url" target="_blank">National Housing Federation</a>
 			</div>
 		</article>
 		<article class="project project--media-left" itemscope itemtype="http://schema.org/SoftwareApplication">
-			<h2 class="project__title" itemprop="name">GroupMe Microsites</h2>
-			<div class="project__body">
-				<figure class="project__media">
-					<img class="" itemprop="image" src="" alt="GroupMe Microsites">
-					<figcaption class="project__media-description">GroupMe Microsites</figcaption>
-				</figure>
-				<div class="project__copy editorial">
+			<figure class="project__media">
+				<img class="project__media-img" itemprop="image" src="/assets/site_images/groupme.jpg" alt="GroupMe Microsites">
+				<figcaption class="project__media-description">GroupMe Microsites</figcaption>
+			</figure>
+			<div class="project__copy">
+				<h2 class="project__title" itemprop="name">GroupMe Microsites</h2>
+				<div class="editorial">
 					<p>I developed prototype software using <a href="https://laravel.com/" target="_blank">Laravel 5</a> with the <a href="https://www.twilio.com/" target="_blank">Twilio</a> SMS API for Microsoft/Skype owned application <a href="https://groupme.com/" target="_blank">GroupMe</a>, to increase their exposure in the UK.</p>
 					<p>I developed the front-end of the live applications, using <a href="http://sass-lang.com/" target="_blank">SASS</a>, HTML and JS.</p>
 				</div>
 			</div>
 		</article>
 		<article class="project project--media-right" itemscope itemtype="http://schema.org/WebSite">
-			<h2 class="project__title" itemprop="name">twobirds Bridesmaid</h2>
-			<div class="project__body">
-				<figure class="project__media">
-					<a href="http://twobirdsbridesmaid.com/" target="_blank"><img class="" itemprop="image" src="" alt="twobirds Bridesmaid"></a>
-					<figcaption class="project__media-description">twobirds Bridesmaid</figcaption>
-				</figure>
-				<div class="project__copy editorial">
+			<figure class="project__media">
+				<a href="http://twobirdsbridesmaid.com/" target="_blank"><img class="project__media-img" itemprop="image" src="/assets/site_images/twobirds.jpg" alt="twobirds Bridesmaid"></a>
+				<figcaption class="project__media-description">twobirds Bridesmaid</figcaption>
+			</figure>
+			<div class="project__copy">
+				<h2 class="project__title" itemprop="name">twobirds Bridesmaid</h2>
+				<div class="editorial">
 					<p>Using <a href="https://craftcms.com/" target="_blank">Craft CMS</a> and their e-commerce plugin <a href="https://craftcommerce.com/" target="_blank">Commerce</a>, I developed both the back and front-end of the twobirds Bridesmaid websites for the US, UK, Canada and Australia.</p>
 					<p>Bespoke development was required to adjust the custom e-commerce requirements, using the <a href="http://www.yiiframework.com/" target="_blank">Yii</a> PHP framework.</p>
-					<a class="project__link" href="http://twobirdsbridesmaid.com/" itemprop="url" target="_blank">twobirds Bridesmaid</a>
 				</div>
+				<a class="project__link" href="http://twobirdsbridesmaid.com/" itemprop="url" target="_blank">twobirds Bridesmaid</a>
 			</div>
 		</article>
 		<article class="project project--media-left" itemscope itemtype="http://schema.org/WebSite">
-			<h2 class="project__title" itemprop="name">Royal Free Hospital</h2>
-			<div class="project__body">
-				<figure class="project__media">
-					<a href="https://www.royalfree.nhs.uk/" target="_blank"><img class="" itemprop="image" src="" alt="Royal Free Hospital"></a>
-					<figcaption class="project__media-description">Royal Free Hospital</figcaption>
-				</figure>
-				<div class="project__copy editorial">
+			<figure class="project__media">
+				<a href="https://www.royalfree.nhs.uk/" target="_blank"><img class="project__media-img" itemprop="image" src="/assets/site_images/royalfree.jpg" alt="Royal Free Hospital"></a>
+				<figcaption class="project__media-description">Royal Free Hospital</figcaption>
+			</figure>
+			<div class="project__copy">
+				<h2 class="project__title" itemprop="name">Royal Free Hospital</h2>
+				<div class="editorial">
 					<p>With <a href="http://electricputty.co.uk/" target="_blank">Electric Putty</a>, I led the development (front and back-end) of the <a href="https://www.royalfree.nhs.uk/" target="_blank">Royal Free London Hospital</a> website.</p>
 					<p>The site was built in <a href="http://expressionengine.com/" target="_blank">ExpressionEngine</a> and <a href="http://sass-lang.com/" target="_blank">SASS</a> was used for the CSS as well as gulp for code compilation.</p>
-					<a class="project__link" href="https://www.royalfree.nhs.uk/" itemprop="url" target="_blank">Royal Free Hospital</a>
 				</div>
+				<a class="project__link" href="https://www.royalfree.nhs.uk/" itemprop="url" target="_blank">Royal Free Hospital</a>
 			</div>
 		</article>
 	</main><!-- /.site-body -->
 
-	<footer class="site-footer site-wrapper site-wrapper--body-text" role="contentinfo">
+	<footer class="site-footer site-wrapper" role="contentinfo">
 		<p>&copy; <?php echo date("Y"); ?> highpass</p>
 		<p>hi at highpass dot co dot uk</p>
 	</footer><!-- /.site-footer -->
