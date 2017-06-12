60f8cbb94d9a180e7699ed38b8900bc75f1bae4d
Rob Hodges <robhodges@gmail.com>
2017-06-12 18:34:11 +0100

    Add google analytics code

--- public/index.php
+++ public/index.php
@@ -128,5 +128,15 @@
 		<p>&copy; <?php echo date("Y"); ?> highpass</p>
 		<p>hi at highpass dot co dot uk</p>
 	</footer><!-- /.site-footer -->
+
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-12818160-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </body>
 </html>
